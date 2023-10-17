262c24f627494e4d5c02a64d5073801ee16d5127
AgsRsoa <agnesrami@yahoo.fr>
2023-10-17 15:17:53 +0200

    Add database connection

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -4,23 +4,41 @@ const UserModel = require('../models/user')
 const pokemons = require('./mock-pokemon')
 const bcrypt = require('bcrypt')
 
-const sequelize = new Sequelize (
-    'pokedex',//nom BDD
-    'root', //id pour accès à BDD root pour Maria DB
-    '', //mdp 
-    {
-        host:'localhost',// 192.168.1.80 (mien) 
-        dialect: 'mariadb',
-        dialectOptions: { timezone: 'Etc/GMT-2'}  
-    ,
-    logging: true
-})
+let sequelize 
+
+if(process.env.NODE_ENV === "production"){
+     sequelize = new Sequelize (
+        'txh0y9l7mjq4nny4',
+        'h8jab69nruszlv3p', 
+        'oukeaz37qh29gc44', 
+        {
+            host:'dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+            dialect: 'mariadb',
+            dialectOptions: { timezone: 'Etc/GMT-2'}  
+        ,
+        logging: true
+    })
+} else {
+     sequelize = new Sequelize (
+        'pokedex',//nom BDD 
+        'root', //id pour accès à BDD root pour Maria DB 'root' par défaut
+        '', //mdp 
+        {
+            host:'localhost',
+            dialect: 'mariadb',
+            dialectOptions: { timezone: 'Etc/GMT-2'}  
+        ,
+        logging: false
+    })
+
+}
+
 
 const Pokemon = PokemonModel(sequelize,DataTypes) //Instanciation du modele PokemonModel
 const User = UserModel(sequelize, DataTypes)
 
 const initDb = ()=>{
-    return sequelize.sync({force:true}).then(_ => {
+    return sequelize.sync().then(_ => {
         pokemons.map(pokemon => {
             Pokemon.create({
                 name:pokemon.name,
